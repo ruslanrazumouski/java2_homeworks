f67a7573183ecfad924426a1d60e8880ac5a28b5
Ruslan Razumouski <ruslanrazumouski@gmail.com>
2020-01-18 21:38:50 +0100

    lesson_01 first commit

---
+++ src/lesson_01/MainCircles.java
@@ -0,0 +1,56 @@
+package ru.gb.jtwo.lone.online.circles;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainCircles extends JFrame {
+    private static final int POS_X = 400;
+    private static final int POS_Y = 200;
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+
+    Sprite[] sprites = new Sprite[10];
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainCircles();
+            }
+        });
+    }
+
+    private MainCircles() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
+        initApplication();
+        GameCanvas canvas = new GameCanvas(this);
+        add(canvas, BorderLayout.CENTER);
+        setTitle("Circles");
+        setVisible(true);
+    }
+
+    private void initApplication() {
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i] = new Ball();
+        }
+    }
+
+    public void onDrawFrame(GameCanvas canvas, Graphics g, float deltaTime) {
+        update(canvas, deltaTime); // obnovlenie // S = v * t
+        render(canvas, g); // otrisovka
+    }
+
+    private void update(GameCanvas canvas, float deltaTime) {
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i].update(canvas, deltaTime);
+        }
+    }
+
+    private void render(GameCanvas canvas, Graphics g) {
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i].render(canvas, g);
+        }
+    }
+
+}
